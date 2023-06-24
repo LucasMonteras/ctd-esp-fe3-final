4ab8d7b2ad2a9589586c8003e1cd340d7a9f4188
LucasMonteras <lucasmonteras@gmail.com>
2023-06-24 00:32:24 -0300

    se realizo link del navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 
 import{Route,Routes} from 'react-router-dom'
-import Navbar from './Components/Navbar';
 import Layout from './Layout/Layout';
-
+import { routes } from './Components/utils/routes';
+import Home from './Routes/Home';
+import Contact from './Routes/Contact'
+import Favs from './Routes/Favs'
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout/>}/>
           <Route path={routes.home} element={<Home/>}/>
+          <Route path={routes.contact} element={<Contact/>}/>
+          <Route path={routes.favs} element={<Favs/>}/>
         </Routes>
       </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,13 +9,15 @@ const Navbar = () => {
   return (
     <nav>
       <h1>DH Odonto</h1>
+      <div className='links'>
       <Link to={routes.home}><h4 className='link'>Home</h4></Link>
-      <Link to={routes.contact}><h4>Contact</h4></Link>
-      <Link to={routes.favs}><h4>Favs</h4></Link>
-      
+      <Link to={routes.contact}><h4 className='link'>Contact</h4></Link>
+      <Link to={routes.favs}><h4 className='link'>Favs</h4></Link>
+      <button className='favButton'>☀ 🌙</button>
+      </div>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+     
     </nav>
   )
 }
